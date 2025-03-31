9354eda42b805846bb34a133ebd5ec81244bbeb4
GeethmaMunasinghe <123428378+GeethmaMunasinghe@users.noreply.github.com>
2025-03-31 20:16:34 +0530

    Priority test complete

--- TestNG_practice/src/test/java/testNG/PriorityTest01.java
+++ TestNG_practice/src/test/java/testNG/PriorityTest01.java
@@ -3,27 +3,27 @@ package testNG;
 import org.testng.annotations.Test;
 
 public class PriorityTest01 {
-    @Test
+    @Test(priority = 0)
     public void testLogin(){
         System.out.println("Login");
     }
 
-    @Test
+    @Test(priority = 1)
     public void testCreate(){
         System.out.println("Create");
     }
 
-    @Test
+    @Test(priority = 2,enabled = false)
     public void testEdit(){
         System.out.println("Edit");
     }
 
-    @Test
+    @Test(priority = 3)
     public void testDelete(){
         System.out.println("Delete");
     }
 
-    @Test
+    @Test(priority = 4)
     public void testLogout(){
         System.out.println("Logout");
     }
