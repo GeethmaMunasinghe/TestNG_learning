cb46b40bc97ef966d226321d43983eb37236e833
GeethmaMunasinghe <123428378+GeethmaMunasinghe@users.noreply.github.com>
2025-03-31 20:53:44 +0530

    create methods in DependsOnTest class

--- TestNG_practice/src/test/java/testNG/DependsOnTest.java
+++ TestNG_practice/src/test/java/testNG/DependsOnTest.java
@@ -1,5 +1,20 @@
 package testNG;
 
-public class DependsOnTest {
+import org.testng.annotations.Test;
 
+public class DependsOnTest {
+    @Test
+    public void oLevel(){
+        System.out.println("O/Level");
+    }
+
+    @Test
+    public void aLevel(){
+        System.out.println("A/Level");
+    }
+
+    @Test
+    public void campus(){
+        System.out.println("Campus");
+    }
 }
