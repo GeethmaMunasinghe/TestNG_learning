1d0aa3bba3798e44533186fd9789df3af825682a
GeethmaMunasinghe <123428378+GeethmaMunasinghe@users.noreply.github.com>
2025-03-31 21:56:48 +0530

    complete DependsOn class

--- TestNG_practice/src/test/java/testNG/DependsOnTest.java
+++ TestNG_practice/src/test/java/testNG/DependsOnTest.java
@@ -1,19 +1,21 @@
 package testNG;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class DependsOnTest {
-    @Test
+    @Test(priority = 0)
     public void oLevel(){
         System.out.println("O/Level");
+        Assert.fail();
     }
 
-    @Test
+    @Test(priority = 1)
     public void aLevel(){
         System.out.println("A/Level");
     }
 
-    @Test
+    @Test(priority = 2)
     public void campus(){
         System.out.println("Campus");
     }
