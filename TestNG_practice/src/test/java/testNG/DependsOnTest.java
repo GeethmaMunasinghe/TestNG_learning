0818454ae2047694c6cd4f67e9ff1dfea9fef492
GeethmaMunasinghe <123428378+GeethmaMunasinghe@users.noreply.github.com>
2025-04-01 04:57:06 +0530

    creating xml file

--- TestNG_practice/src/test/java/testNG/DependsOnTest.java
+++ TestNG_practice/src/test/java/testNG/DependsOnTest.java
@@ -4,18 +4,20 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class DependsOnTest {
-    @Test()
+    @Test(priority = 0)
     public void oLevel(){
         System.out.println("O/Level");
-        Assert.fail();
+        //Assert.fail();
     }
 
-    @Test(dependsOnMethods = "oLevel")
+    //@Test(dependsOnMethods = "oLevel")
+    @Test(priority = 1)
     public void aLevel(){
         System.out.println("A/Level");
     }
 
-    @Test(dependsOnMethods = {"oLevel","aLevel"},alwaysRun = true)
+    //@Test(dependsOnMethods = {"oLevel","aLevel"},alwaysRun = true)
+    @Test(priority = 2)
     public void campus(){
         System.out.println("Campus");
     }
