781cc69b8eb826289d79deec721b5c529079227b
GeethmaMunasinghe <123428378+GeethmaMunasinghe@users.noreply.github.com>
2025-04-01 22:13:28 +0530

    complete equalsCheck assertion

--- TestNG_practice/src/test/java/testNG/AssertionTest.java
+++ TestNG_practice/src/test/java/testNG/AssertionTest.java
@@ -1,16 +1,22 @@
 package testNG;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class AssertionTest {
-    String name="Geethma";
+    String actualValue="Geethma";
 
-    @Test
+    @Test(priority = 0)
     public void valuesEqualCheck(){
-        if (name.equals("Geethma")){
+        /*if (name.equals("Geethma")){
             System.out.println("Name is equal.");
         }else {
             System.out.println("Name is not equal.");
-        }
+        }*/
+        String expectedValue="Geethma";
+        System.out.println("Prior to equalsCheck assertion ");
+        Assert.assertEquals(actualValue,expectedValue,"Value miss matched");
+        System.out.println("After equalsCheck assertion ");
     }
+
 }
