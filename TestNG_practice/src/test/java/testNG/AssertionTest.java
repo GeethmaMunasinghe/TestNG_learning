7f5374115f461dc3f65ffe157cc11fc94b856d3d
GeethmaMunasinghe <123428378+GeethmaMunasinghe@users.noreply.github.com>
2025-04-01 23:00:53 +0530

    complete hard assert

--- TestNG_practice/src/test/java/testNG/AssertionTest.java
+++ TestNG_practice/src/test/java/testNG/AssertionTest.java
@@ -2,8 +2,10 @@ package testNG;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class AssertionTest {
+    SoftAssert softAssert=new SoftAssert();
     String actualValue="Geethma";
 
     @Test(priority = 0)
@@ -14,6 +16,7 @@ public class AssertionTest {
             System.out.println("Name is not equal.");
         }*/
         String expectedValue="Geethma";
+        //String expectedValue="Geethma123";
         System.out.println("Prior to equalsCheck assertion ");
         Assert.assertEquals(actualValue,expectedValue,"Value miss matched");
         System.out.println("After to equalsCheck assertion ");
@@ -22,7 +25,7 @@ public class AssertionTest {
     @Test(priority = 1)
     public void valuesNotEqualCheck(){
         String expectedValue="Learning TestNG";
-        System.out.println("Prior to equalsCheck assertion ");
+        System.out.println("Prior to valuesNotEqualCheck assertion ");
         Assert.assertNotEquals(actualValue,expectedValue,"Value matched");
         System.out.println("After to valuesNotEqualCheck assertion ");
     }
