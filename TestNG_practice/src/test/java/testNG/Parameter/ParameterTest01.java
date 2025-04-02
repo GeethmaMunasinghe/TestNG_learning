f18a9580af1ba148ed1eaca928c79155176ef7f6
GeethmaMunasinghe <123428378+GeethmaMunasinghe@users.noreply.github.com>
2025-04-02 22:53:04 +0530

    parameter test contd.

--- TestNG_practice/src/test/java/testNG/Parameter/ParameterTest01.java
+++ TestNG_practice/src/test/java/testNG/Parameter/ParameterTest01.java
@@ -1,9 +1,11 @@
 package testNG.Parameter;
 
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class ParameterTest01 {
     @Test
+    @Parameters({"val1","val2"})
     public void Sum(int v1,int v2){
         int finalsum=v1+v2;
         System.out.println("The final sum of the given values: "+finalsum);
--- TestNG_practice/src/test/java/testNG/Parameter/ParameterTest02.java
+++ TestNG_practice/src/test/java/testNG/Parameter/ParameterTest02.java
@@ -1,11 +1,13 @@
 package testNG.Parameter;
 
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class ParameterTest02 {
     @Test
+    @Parameters({"val1","val2"})
     public void Diff(int v1,int v2){
         int finalDiff=v1-v2;
-        System.out.println("Final value ");
+        System.out.println("Final difference of the given value is: "+finalDiff);
     }
 }
