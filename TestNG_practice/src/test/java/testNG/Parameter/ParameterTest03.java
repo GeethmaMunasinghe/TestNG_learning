99d2b97ab840cdb00871788933f508a246c842d6
GeethmaMunasinghe <123428378+GeethmaMunasinghe@users.noreply.github.com>
2025-04-02 23:19:20 +0530

    browserExecution method contd.

--- TestNG_practice/src/test/java/testNG/Parameter/ParameterTest03.java
+++ TestNG_practice/src/test/java/testNG/Parameter/ParameterTest03.java
@@ -1,8 +1,13 @@
 package testNG.Parameter;
 
 
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
 public class ParameterTest03 {
 
+    @Test
+    @Parameters("browser")
     public void browserExecution(String browser){
         System.out.println("TestNG xml file passed browser value is :"+browser);
         if (browser.equalsIgnoreCase("chrome")){
