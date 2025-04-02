61644d9ed22f5afd07ea52979ae92e950f98c1ee
GeethmaMunasinghe <123428378+GeethmaMunasinghe@users.noreply.github.com>
2025-04-02 23:09:54 +0530

    browserExecution method

--- TestNG_practice/src/test/java/testNG/Parameter/ParameterTest01.java
+++ TestNG_practice/src/test/java/testNG/Parameter/ParameterTest01.java
@@ -1,12 +1,13 @@
 package testNG.Parameter;
 
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class ParameterTest01 {
     @Test
     @Parameters({"val1","val2"})
-    public void Sum(int v1,int v2){
+    public void Sum(@Optional("75") int v1, @Optional("4") int v2){
         int finalsum=v1+v2;
         System.out.println("The final sum of the given values: "+finalsum);
     }
--- TestNG_practice/src/test/java/testNG/Parameter/ParameterTest03.java
+++ TestNG_practice/src/test/java/testNG/Parameter/ParameterTest03.java
@@ -1,4 +1,16 @@
 package testNG.Parameter;
 
+
 public class ParameterTest03 {
+
+    public void browserExecution(String browser){
+        System.out.println("TestNG xml file passed browser value is :"+browser);
+        if (browser.equalsIgnoreCase("chrome")){
+            System.out.println("Browser should open with : chrome.");
+        }else if (browser.equalsIgnoreCase("edge")){
+            System.out.println("Browser should open with : edge.");
+        }else {
+            System.out.println("Browser should open with : Firefox");
+        }
+    }
 }
