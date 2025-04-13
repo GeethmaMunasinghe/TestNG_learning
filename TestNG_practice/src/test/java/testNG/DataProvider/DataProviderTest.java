0cbb6452abc925b421c54efb2c22f71c48fbc02b
GeethmaMunasinghe <123428378+GeethmaMunasinghe@users.noreply.github.com>
2025-04-13 08:21:11 +0530

    create getData Method.

--- TestNG_practice/src/test/java/testNG/DataProvider/DataProviderTest.java
+++ TestNG_practice/src/test/java/testNG/DataProvider/DataProviderTest.java
@@ -1,4 +1,11 @@
 package testNG.DataProvider;
 
 public class DataProviderTest {
+
+    public Object [][] getData(){
+        Object[][] data= {
+                {"abc@gmail.com","abc123"},{"def@gmail.com","abc123"},{"xyz@gmail.com","xyz123"}
+        };
+        return data;
+    }
 }
